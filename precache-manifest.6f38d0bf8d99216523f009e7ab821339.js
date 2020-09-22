8d936d5eb159de2af0001cf565d731278f58386b
Ujjwal <aujjwaljain@gmail.com>
2020-09-22 22:01:08 +0530

    Updates

---
+++ precache-manifest.6f38d0bf8d99216523f009e7ab821339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482dfb2f5eeec8611bb306d5d675c3d0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "95d3686743d26a6e4167",
+    "url": "/robofriends/static/css/main.9e036412.chunk.css"
+  },
+  {
+    "revision": "47d27e2e9573f88eb87d",
+    "url": "/robofriends/static/js/2.0bc9dc31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0bc9dc31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d3686743d26a6e4167",
+    "url": "/robofriends/static/js/main.bc21e446.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
